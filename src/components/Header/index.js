332c9a1e5fdb2657f052992a53961d85836db333
Ganesh Kumar <bojjaganeshkumar1@gmail.com>
2024-02-16 22:10:53 +0530

    added Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const Header = () => (
+  <nav className="header-container">
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2P3vJ2I9DdbeSBr9QXguaiEnRpatUvFp7qg&usqp=CAU"
+      alt="website logo"
+      className="website-logo"
+    />
+  </nav>
+);
+
+export default Header;
