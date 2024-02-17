7120d650bbb4d5f326ee70f8eec7325ba4a7da39
Ganesh Kumar <bojjaganeshkumar1@gmail.com>
2024-02-17 16:51:17 +0530

    added more components

--- src/components/AppBody/index.js
+++ src/components/AppBody/index.js
@@ -1,10 +1,18 @@
 import HomePage from "../HomePage";
+import WorkWithUs from "../WorkWithUs";
+import MeetAhead from "../MeetAheadCard";
+import Vacancies from "../Vacancies";
+import StartTestPage from "../StartTestPage";
 
 import "./index.css";
 
 const AppBody = () => (
   <div className="app-body">
     <HomePage />
+    <MeetAhead />
+    <WorkWithUs />
+    <Vacancies />
+    <StartTestPage />
   </div>
 );
 
